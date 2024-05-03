3677741322661abab88ccc4506f28545f1efa936
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-05-03 19:42:57 +0200

    Rewriten code to use inframe instead of anchor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+// object for buttons to adjust menu hidding after click
+const buttons=[
+  document.getElementById("b-welcome"),
+  document.getElementById("b-projects"),
+  document.getElementById("b-contact")
+]
+
+//iframe elements
+const iframeContainer = document.getElementById("iframe-container")
+const iframe = document.getElementById("iframe")
+const visitpage = document.getElementById("visitpage")
+
+// unckeck menu
+const uncheck= function(){document.getElementById("mobile-navbar").checked=false}
+
 // Link to the projects container
 const projects =document.getElementById("projects-content")
 
@@ -9,14 +24,34 @@ async function getPortfolio() {
     return portfolio
   }
 
-// Post fetched conetent
-function postPortfoilio(portfolio){
-    for(let i=0; i<portfolio.projects.length; i++)
-    projects.innerHTML += "<a id='"+portfolio.projects[i].id+"' target='_blank' style='background-image: url(./assets/img/"+portfolio.projects[i].img+");' href='"+portfolio.projects[i].url+"'><p>"+portfolio.projects[i].name+"</p></a>"
+// Post fetched content and add iframe
+function postPortfolio(portfolio){
+    for(let i=0; i<portfolio.projects.length; i++){
+      projects.innerHTML+=`
+      <div id="${portfolio.projects[i].id}" style="background-image: url(./assets/img/${portfolio.projects[i].img}"><p>${portfolio.projects[i].name}</p></div>
+      `
+    }
+    for(let i=0; i<portfolio.projects.length; i++){
+    document.getElementById(portfolio.projects[i].id).onclick= function(){
+      iframe.setAttribute("src",portfolio.projects[i].url)
+      visitpage.setAttribute("href",portfolio.projects[i].url)
+      iframeContainer.classList.toggle("hidden")
+    }
+  }
 }
 
   
   
 
 //RUN FUNCTION//
-getPortfolio().then(data => postPortfoilio(data))
+getPortfolio().then(data => postPortfolio(data))
+
+//Buttons adjustment| Menu hidding after click //
+for(let element of buttons){
+  element.onclick= ()=>{uncheck()}
+}
+
+//hide iframe
+iframeContainer.addEventListener("click",()=>{
+  iframeContainer.classList.toggle("hidden")
+})
